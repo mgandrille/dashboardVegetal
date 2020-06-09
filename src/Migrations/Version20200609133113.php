5ed7972d357355b299a64e874a872392d8d17a6c
DarkSalmon <roffat.theau@outlook.com>
2020-06-09 16:02:37 +0200

    registration and login

--- src/Migrations/Version20200609133113.php
+++ src/Migrations/Version20200609133113.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200609131545 extends AbstractMigration
+final class Version20200609133113 extends AbstractMigration
 {
     public function getDescription() : string
     {
