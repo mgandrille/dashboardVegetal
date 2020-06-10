cb3989abca8f6d0ee505394c603686771c08b0ab
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-06-10 10:03:52 +0200

    ajout de dépendaces : bootstrap - jquery - poppers.js dans node Module. Feuille de style générale dans front/src qui rethème les classes bootstrap + structure et début mise en page homepage

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,25 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-  * {
-    box-sizing: border-box;
-  }
-
-  /* colors */
-  $blue: #1A432A;
-  $gray-600: #C98E3E;
-  $green :#228749;
-  $gray-100: #EDECEA;
-
-  /* shapes */
-  $border-radius: .12rem;
-  @import "../../node_modules/bootstrap/scss/bootstrap.scss";
-</style>
+<style lang="scss" src="./styles.scss"></style>
