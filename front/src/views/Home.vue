e1db377cfe71d45a6901cb27e3571654b6a74261
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-06-10 10:40:56 +0200

    projet en avnt integration jquery

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -11,21 +11,21 @@
                         <div class="col-sm-12">[navbar]</div>
                     </div>
 
-                    <div class="title row mt-5">
+                    <div class="title row mt-5 p-3">
                         <div class="col-lg-12">
                             <p>Suivez l'évolution de vos plantes avec</p>
                             <h1>PlantBoard</h1>
                         </div>
                     </div>
 
-                    <div class="content row">
+                    <div class="content row p-3">
                         <div class="col-lg-12">
                             <p>Suivez, arrosez et planifiez votre entretient de plantes au quotidien grâce à ce gestionnaire de plantes de balcons et d'intérieurs. Vous venez d'acquérir une plante ? 
                                 <a href="#">Inscrivez-vous</a> pour créer votre dashboard.</p>
                         </div>
                     </div>
 
-                    <div class="apperçu row">
+                    <div class="apperçu row p-3">
                         <div class="col-lg-6">[card-dashboard]</div>
                         <div class="col-lg-6">[card-dashboard]</div>
                     </div>
