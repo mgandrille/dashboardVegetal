8bd2f7cb0dfde418fb2a9cfc320a1a297cf9729a
marie gandrille <mgandrille@yahoo.fr>
2020-06-10 11:11:28 +0200

    Ajout de la basser de recherche

---
+++ front/src/components/BarreRecherche.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <form class="form-inline" action="#" method="get">
+<!-- **** Roujouter le fichier de recherche **** -->
+            <input class="form-control mr-sm-2" type="search" name="search" placeholder="Rechercher une plante" aria-label="Search" required>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
+        </form>
+    </nav>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+
+<style>
+
+</style>
