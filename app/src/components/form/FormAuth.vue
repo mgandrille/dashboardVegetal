b16c2b65d02ba921035f6c7bd280cfa9a7669ffb
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-06-11 10:33:52 +0200

    ajout view Authentification + composants formulaires inscription et connexion

---
+++ app/src/components/form/FormAuth.vue
@@ -0,0 +1,15 @@
+<template>
+	<form>
+		<div class="form-group">
+			<label for="exampleInputEmail1">Adresse email</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+			<small id="emailHelp" class="form-text text-muted">Cette adresse ne sera pas partagée</small>
+		</div>
+		<div class="form-group">
+			<label for="exampleInputPassword1">Mot de passe</label>
+			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		</div>
+
+		<button type="submit" class="btn btn-primary">Créer mon compte</button>
+	</form>
+</template>
