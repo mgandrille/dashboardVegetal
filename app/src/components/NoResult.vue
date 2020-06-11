0d9ec803368b35f62407b8be98d2eb450a685305
marie gandrille <mgandrille@yahoo.fr>
2020-06-11 11:41:00 +0200

    Ajout composant pas résultat + page 404

---
+++ app/src/components/NoResult.vue
@@ -0,0 +1,20 @@
+<template>
+    
+    <div class="container d-flex justify-content-center">
+
+            <img src="https://media.giphy.com/media/SVU9KcXqmIx72Ody0N/200w_d.gif" alt="">
+            <!-- <img src="{assets/img/page404.gif}" alt="giffy plante triste"> -->
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ app/src/views/Page404.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="page404">
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="container mt-4 text-center ">
+                    <div class="title mt-5 p-3">
+
+                        <h1 class="text-uppercase text-center">oh non...</h1>
+                        <h2 class="text-center">... Vous êtes sur la page 404...</h2>
+                    </div>
+
+                    <NoResult/>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import NoResult from '../components/NoResult.vue';
+export default {
+    name: "page404",
+    components: {
+        NoResult
+    }
+
+}
+</script>
+
+<style>
+
+</style>
