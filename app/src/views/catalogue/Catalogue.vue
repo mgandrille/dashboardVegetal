21f033f37604b3f7beac882a23dd0dd986f7e8ab
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-06-13 12:14:47 +0200

    ajout du package de scrolling permettant de faire des ancres et scrolls dans les sections de l'application

--- app/src/views/catalogue/Catalogue.vue
+++ app/src/views/catalogue/Catalogue.vue
@@ -1,35 +1,61 @@
 <template>
-	<div class="home">
+    <div class="home">
         <div class="container-fluid">
             <div class="row">
                 <!-- left-side banner -->
                 <div class="filtrage col-lg-2 position-fixed">
-					<Filtrage v-on:get-params="searchParams = $event"/>
-				</div>
+                    <Filtrage v-on:get-params="searchParams = $event" />
+                </div>
 
                 <!-- main content -->
-                <main class="main-content col-lg-10 offset-lg-2 container-lg bg-light">
-                    
+                <main
+                id="top-page"
+                    class="main-content col-lg-10 offset-lg-2 container-lg bg-light"
+                >
                     <div class="searchbar row p-3 justify-content-center">
-                            <BarreRecherche />
+                        <BarreRecherche />
                     </div>
 
-					<div class="title row mt-5 p-3">
-						<div class="col-lg-12 d-flex flex-wrap justify-content-center">
-                            <PlantCard 	v-for="plant in plants" 
-										:key="plant.id"
-										:plantItem="plant" >
-								<template v-slot:plant-name>
-									<h5 class="card-title">{{ plant.name }}</h5>
-								</template>
-								<template v-slot:plant-description>
-									<p class="card-text">{{ plant.description }}</p>
-								</template>
-							</PlantCard>
-						</div>
+                    <div class="title row mt-5 p-3">
+                        <div class="col-lg-12 d-flex flex-wrap justify-content-center">
+                            <PlantCard
+                                v-for="(plant, index) in displayedPlants"
+                                :key="index"
+                                :plantItem="plant"
+                            >
+                                <template v-slot:plant-name>
+                                    <h5 class="card-title">{{ plant.name }}</h5>
+                                </template>
+                                <template v-slot:plant-description>
+                                    <p class="card-text">{{ plant.description }}</p>
+                                </template>
+                            </PlantCard>
+                        </div>
                     </div>
-
                 </main>
+                <div class="clearfix btn-group col-md-2 offset-md-5">
+                    <button
+                        type="button"
+                        class="btn btn-primary"
+                        v-if="page != 1"
+                        @click="page--"
+                    >previous</button>
+                    <button
+                        type="button"
+                        class="btn btn-primary"
+                        v-for="(pageNumber, index) in pages.slice(page-1, page+5)"
+                        :key="index"
+                        @click="page = pageNumber"
+                    >{{pageNumber}}</button>
+                    <button
+                        to="'#top-page'"
+                        type="button"
+                        @click="page++"
+                        v-scroll-to="'#top-page'"
+                        v-if="page < pages.length"
+                        class="btn btn-primary"
+                    >next</button>
+                </div>
             </div>
         </div>
     </div>
@@ -39,31 +65,67 @@
 <style scoped lang="scss" src="./catalogue.scss"></style>
 
 <script>
-import BarreRecherche from '../../components/BarreRecherche.vue';
-import PlantCard from '../../components/plant-card/PlantCard.vue';
-import Filtrage from '../../components/Filtrage.vue';
+import BarreRecherche from "../../components/BarreRecherche.vue";
+import PlantCard from "../../components/plant-card/PlantCard.vue";
+import Filtrage from "../../components/Filtrage.vue";
 
 export default {
-	name: 'Catalogue',
-	components: {
-		BarreRecherche,
-		PlantCard,
-		Filtrage
-	},
+    name: "Catalogue",
+    components: {
+        BarreRecherche,
+        PlantCard,
+        Filtrage
+    },
 
-	data() {
-		return {
-			plants: [],
-			searchParams: ''
-		}
-	},
+    data() {
+        return {
+            plants: [],
+            searchParams: "",
+            page: 1,
+            perPage: 10,
+            pages: []
+        };
+    },
 
-	created() {
-		this.$http.get('api/plantes')
-			.then((result) => {
-				this.plants = result.data;
-			})
-	}
+    methods: {
+        getPlants() {
+            this.$http.get("api/plantes").then(result => {
+                this.plants = result.data;
+            });
+        },
+
+        setPages() {
+            let nbPages = Math.ceil(this.plants.length / this.perPage);
+            for (let i = 1; i <= nbPages; i++) {
+                this.pages.push(i);
+            }
+        },
+
+        paginate(plants) {
+            let page = this.page;
+            let perPage = this.perPage;
+            let from = page * perPage - perPage;
+            let to = page * perPage;
+
+            return plants.slice(from, to);
+        }
+    },
+
+    computed: {
+        displayedPlants() {
+            return this.paginate(this.plants);
+        }
+    },
+
+    watch: {
+        plants() {
+            this.setPages();
+        }
+    },
+
+    created() {
+        this.getPlants();
+    }
 };
 </script>
 
