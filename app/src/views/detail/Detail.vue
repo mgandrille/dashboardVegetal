f9df2fd3cd8e72d6553e399874e15f55a54c842d
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-06-14 16:17:10 +0200

    mise en page liste d'informations pour une plante

--- app/src/views/detail/Detail.vue
+++ app/src/views/detail/Detail.vue
@@ -7,40 +7,85 @@
 
                 <!-- main content -->
                 <main class="main-content col-lg-5 offset-lg-7 container-lg bg-light">
-                    
                     <div class="title row mt-5 p-3">
                         <div class="col-lg-12">
-                            <h2>Name</h2>
+                            <h2>Ficcus</h2>
                         </div>
                     </div>
 
                     <div class="content row p-3">
                         <div class="col-lg-12">
-							<p>Description</p>
-							<hr>
+                            <p>Description</p>
+                            <hr />
                             <ul>
-								<li>Difficulté :</li>
-								<li>Espèce :</li>
-								<li>Catégorie :</li>
-								<li>Type : </li>
-								<li>Floraison : </li>
-							</ul>
+                                <li>Difficulté : Difficile</li>
+                                <li>Espèce : Plantosous</li>
+                                <li>Catégorie : Plantimus</li>
+                                <li>Type : Planticus</li>
+                                <li>Floraison : Juillet</li>
+                            </ul>
                         </div>
                     </div>
                 </main>
             </div>
 
-			<footer class="row footer-detail position-fixed fixed-bottom">
-				<div class="col-12">
-					<ul class="text-light">
-						<li>Arrosage :</li>
-						<li>Ensoleillement :</li>
-						<li>Boutage :</li>
-						<li>Rempotage :</li>
-						<li>Taille :</li>
-					</ul>
-				</div>
-			</footer>
+            <footer class="footer-detail fixed-bottom p-3">
+                <div class="row informations text-light">
+
+                    <div class="col-md-4">
+                        <div class="col-md-auto">
+                            <h4>
+                                Arrosage :
+                                <span>Fréquent</span>
+                            </h4>
+                        </div>
+
+                        <hr>
+
+                        <div class="col-md-auto">
+                            <h4>
+                                Ensoleillement :
+                                <span>Fort</span>
+                            </h4>
+                        </div>
+
+                        <hr>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="col-md-auto">
+                            <h4>
+                                Boutage :
+                                <span>Mai</span>
+                            </h4>
+                        </div>
+
+                        <hr>
+
+                        <div class="col-md-auto">
+                            <h4>
+                                Rempotage :
+                                <span>Avril</span>
+                            </h4>
+                        </div>
+
+                        <hr>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="col-md-auto">
+                            <h4>
+                                Taille :
+                                <span>Juin</span>
+                            </h4>
+                        </div>
+
+                        <hr>
+                    </div>
+
+                </div>
+            </footer>
+
         </div>
     </div>
 </template>
@@ -50,7 +95,6 @@
 
 
 <script>
-
 export default {
     name: "Detail"
 };
