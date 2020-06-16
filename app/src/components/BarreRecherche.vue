c1f7a541e73855980833a274ee50dbe3e64188fb
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-06-16 20:36:43 +0200

    relecture requête sur la barre de recherche

--- app/src/components/BarreRecherche.vue
+++ app/src/components/BarreRecherche.vue
@@ -1,8 +1,13 @@
 <template>
     <nav class="navbar navbar-light bg-light">
         <form class="form-inline my-2 my-lg-0" action="#" method="get">
-            <input class="form-control mr-sm-2" type="search" name="search" placeholder="Rechercher une plante" aria-label="Search" required>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
+            <input class="form-control mr-sm-2" type="search" name="search" placeholder="Rechercher une plante" aria-label="Search" v-model="nameSelected" required>
+            <button class="btn btn-outline-success my-2 my-sm-0" 
+                    type="submit"
+                    @click.prevent="$emit('get-nameParam',  {
+                    name: getParams(params.nameSelected),
+                    filter: true
+                })"><i class="fa fa-search"></i></button>
         </form>
     </nav>
 </template>
@@ -10,6 +15,25 @@
 <script>
 
 export default {
+    name: 'BarreRecherche',
+
+    data() {
+        return {
+            params: {
+                nameSelected: "",
+            }
+        }
+    },
+
+    methods: {
+        getParams(param) {
+            if (param === "") {
+                return null;
+            }
+
+            return param;
+        }
+    }
 
 }
 </script>
