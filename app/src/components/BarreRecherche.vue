959a07202489640c406e3f6ff7eedb36ec3d5b05
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-06-16 22:06:44 +0200

    la search bar et la navbar de la page catalogue fonctionnent ensemble

--- app/src/components/BarreRecherche.vue
+++ app/src/components/BarreRecherche.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="navbar navbar-light bg-light">
         <form class="form-inline my-2 my-lg-0" action="#" method="get">
-            <input class="form-control mr-sm-2" type="search" name="search" placeholder="Rechercher une plante" aria-label="Search" v-model="nameSelected" required>
+            <input class="form-control mr-sm-2" type="search" name="search" placeholder="Rechercher une plante" aria-label="Search" v-model="params.nameSelected" required>
             <button class="btn btn-outline-success my-2 my-sm-0" 
                     type="submit"
-                    @click.prevent="$emit('get-nameParam',  {
+                    @click.prevent="$emit('get-name-param',  {
                     name: getParams(params.nameSelected),
                     filter: true
                 })"><i class="fa fa-search"></i></button>
