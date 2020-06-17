c923a327380b89accd5ee3adca6ecc5db523b23a
DarkSalmon <roffat.theau@outlook.com>
2020-06-17 12:36:43 +0200

    redirection on login HS

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,6 +24,10 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        if($error){
+            return $this->render('homepage.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        }
+
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
