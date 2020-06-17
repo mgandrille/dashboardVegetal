1addff2a61d669adb235006daa95d8b87393bbb7
marie gandrille <mgandrille@yahoo.fr>
2020-06-17 14:06:41 +0200

    Bouton ajouter est aligné

--- app/src/components/plant-card/PlantCard.vue
+++ app/src/components/plant-card/PlantCard.vue
@@ -18,9 +18,9 @@
             </div>
         </div>
 
-		<footer class="m-2 text-right">
+		<footer class="my-2 text-right">
                 <router-link :to="{ path: '../plante/detail/' + plant.id }" class="btn btn-primary ml-auto" >Voir</router-link>
-                <button class="btn btn-primary ml-auto d-none" @click.prevent="deletePlant()" v-bind:class="{ 'd-block': inDashboard }">Supprimer</button>
+                <button class="btn btn-primary ml-auto d-none" @click.prevent="deletePlant()" v-bind:class="{ 'd-inline-block': inDashboard }">Supprimer</button>
                 <button class="btn btn-primary ml-auto" @click.prevent="addPlant()" v-bind:class="{ disabled: disable, 'd-none': inDashboard }">Ajouter +</button>
         </footer>
     </div>
