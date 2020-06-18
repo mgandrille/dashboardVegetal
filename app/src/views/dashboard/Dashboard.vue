f9366e84c593d882c31d06b59b4da8243690791d
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-06-18 11:32:36 +0200

    gestion de l'affichage de l'arrosage de plante si pas de plante a arroser

--- app/src/views/dashboard/Dashboard.vue
+++ app/src/views/dashboard/Dashboard.vue
@@ -9,11 +9,12 @@
                 <hr />
             </div>
 
-            <div class="arrosage col-lg-9 mt-3">
+            <div class="arrosage col-lg-9 mt-3"  >
+
                 <div class="col-12">
                     <h2 class="mb-3">Prochain arrosage</h2>
                     <p>Voici les plantes à entretenir en priorité</p>
-                    <div class="arrosage-plante row bg-primary justify-content-center pt-3">
+                    <div class="arrosage-plante row bg-primary justify-content-center pt-3" v-if=" Object.keys(plants).length">
                         <div v-for="plant in plants" :key="plant.id">
                             <div
                                 v-if="plant.watering.timeFrequency == 259200"
@@ -24,15 +25,23 @@
                                 >
                                 </PlantCard>
                             </div>
+
+                            <div v-else>
+                                <p class=" text-light text-center">Aucune plante a arroser pour le moment</p>
+                            </div>
                         </div>
+                    </div>
+
+                    <div v-else class="arrosage-plante row bg-primary justify-content-center pt-3">
+                        <p class="text-light text-center">Aucune plante a arroser pour le moment</p>
                     </div>
                 </div>
 
                 <hr />
 
-                <div class="col-12">
+                <div class="col-12" >
                     <h2 class="mb-3">Toutes vos plantes</h2>
-                    <p>Un apperçu de toutes les plantes que vous avez</p>
+                    <p>Un apperçu de toutes les plantes que vous avez :</p>
 
                     <div class="title row mt-5 p-3" v-if="userLogged != null && userLogged.dashboard.id == $route.params.id">
                         <div class="col-lg-12 d-flex flex-wrap justify-content-center">
@@ -48,7 +57,7 @@
                         </div>
                     </div>
 
-                    <div class="title row mt-5 p-3" v-else>
+                    <div class="title row mt-5 p-3" v-else-if="userLogged != null">
                         <div class="col-lg-12 d-flex flex-wrap justify-content-center">
                             <PlantCard
                                 v-for="(plant, index) in plants"
@@ -57,6 +66,12 @@
                                 :inDashboard="inDashboard"
                             >
                             </PlantCard>
+                        </div>
+                    </div>
+
+                    <div v-else class="title row mt-5 p-3">
+                        <div class="col-lg-12 d-flex flex-wrap justify-content-center">
+                            <p>Vous n'avez ajouté plante pour le moment !</p>
                         </div>
                     </div>
 
