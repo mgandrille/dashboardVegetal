9984034c63d687b064cc74519aece5e52e32c0be
DarkSalmon <roffat.theau@outlook.com>
2020-06-18 12:33:32 +0200

    hover fix

--- app/src/components/plant-card/PlantCard.vue
+++ app/src/components/plant-card/PlantCard.vue
@@ -1,4 +1,5 @@
 <template>
+    <router-link :to="{ path: '../plante/detail/' + plant.id }" tag="div">
         <div
             :class="classes"
             @mouseover="hoverOver"
@@ -6,13 +7,8 @@
             class="animCard card m-2 m-lg-5 col-12 col-md-auto"
             style="width: 18rem;"
         >
-            <div
-                router-link
-                :to="{ path: '../plante/detail/' + plant.id }"
-                tag="div"
-                class="d-flex flex-md-column"
-            >
-                <router-link :to="{ path: '../plante/detail/' + plant.id }" tag="img"
+            <div class="d-flex flex-md-column">
+                <img
                     v-bind:src="'http://localhost:8888/uploads/pictures/' + plant.picture"
                     class="card-img-top p-3"
                     alt="image"
@@ -22,46 +18,50 @@
                         class="alert alert-success alert-dismissible fade show d-none"
                         v-bind:class="{ 'd-block': isAlert }"
                         role="alert"
-                    >La plante a été ajoutée à votre <router-link :to="{ path: `/dashboard/${userLogged.dashboard.id}` }" class="font-weight-bold"> Dashboard </router-link> !</div>
+                    >
+                        La plante a été ajoutée à votre Dashboard !
+                        <button
+                            type="button"
+                            class="close"
+                            data-dismiss="alert"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
 
-                    <router-link :to="{ path: '../plante/detail/' + plant.id }" class="card-title" tag="h5">{{ plant.name }}</router-link>
+                    <h5 class="card-title">{{ plant.name }}</h5>
                     <p class="card-text">{{ plant.description.substring(0, 85) }}...</p>
                 </div>
             </div>
-
-            <footer v-if="userLogged != null" class="footer my-2 text-right is-hidden">
-                <button
-                    class="btn btn-secondary ml-auto d-none"
-                    @click.prevent="deletePlant()"
-                    v-bind:class="{ 'd-inline-block': inDashboard }"
-                >Supprimer</button>
-                <button
-                    class="btn btn-primary ml-auto"
-                    @click.prevent="addPlant()"
-                    v-bind:class="{ disabled: isDisable, 'd-none': inDashboard }"
-                    :disabled="isDisable"
-                >Ajouter +</button>
-            </footer>
             
-        </div>
+		<footer v-if="userLogged != undefined" class="my-2 text-right">
+                
+                <button class="btn btn-primary ml-auto d-none" @click.prevent="deletePlant()" v-bind:class="{ 'd-inline-block': inDashboard }">Supprimer</button>
+                <button class="btn btn-primary ml-auto" @click.prevent="addPlant()" v-bind:class="{ disabled: disable, 'd-none': inDashboard }" :disabled="disable">Ajouter +</button>
+        </footer>
+    </div>
+
+    </router-link>
+
 </template>
 
 <script>
 export default {
     name: "PlantCard",
     props: [
-        "plantSrcImg",
-        "plant",
-        "userLogged",
-        "userPlantes",
-        "inDashboard",
-        "dashboard",
-        "toArrose"
+        'plantSrcImg',
+        'plant',
+        'userLogged',
+        'userPlantes',
+        'inDashboard',
+        'dashboard'
     ],
+
 
     data() {
         return {
-            isDisable: false,
+            disable: false,
             inDashboard: false,
             classes: [],
             isAlert: false
@@ -69,19 +69,13 @@ export default {
     },
 
     created() {
-        this.$http.get("api/user").then(result => {
-            if (Object.keys(result.data).length) {
-                this.userLogged = result.data;
-            } else {
-                this.userLogged = null;
-            }
-        });
         this.isDisabled();
+        
     },
 
     methods: {
         addPlant() {
-            if (this.userLogged != null || this.userPlantes != null) {
+            if (this.userLogged != undefined || this.userPlantes != undefined) {
                 this.$http
                     .get(
                         "dashboard/add/" +
@@ -90,38 +84,32 @@ export default {
                             this.plant.id
                     )
                     .then(() => {
-                        return [(this.isAlert = true), (this.isDisable = true)];
+                        return [(this.isAlert = true), (this.disable = true)];
                     });
             }
         },
         deletePlant() {
-            this.$http
-                .get(
-                    "dashboard/remove/" +
-                        this.userLogged.dashboard.id +
-                        "/" +
-                        this.plant.id
-                )
-                .then(() => {
-                    console.log("plante supprimée");
-                    window.location.reload();
-                });
+            this.$http.get('dashboard/remove/' + this.userLogged.dashboard.id + '/' + this.plant.id)
+            .then(() => {
+                console.log('plante supprimée');
+                window.location.reload();
+            })
         },
-        hoverOver() {
-            this.classes.push("animated");
+        hoverOver(){
+            this.classes.push('animated')
         },
-        hoverOut() {
-            this.classes = [];
-        }
+        hoverOut(){
+            this.classes = []
+        },
     },
 
     computed: {
         isDisabled: function() {
-            if (this.userLogged != null || this.userPlantes != null) {
+            if (this.userLogged != undefined || this.userPlantes != undefined) {
                 this.userPlantes.forEach(plante => {
                     if (this.plant.id === plante.id) {
                         this.inDashboard = true;
-                        return (this.isDisable = true);
+                        return (this.disable = true);
                     }
                 });
             }
