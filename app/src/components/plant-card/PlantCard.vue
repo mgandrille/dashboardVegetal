72dd680fd5557baa6a20c419282c6bdbeea964d2
marie gandrille <mgandrille@yahoo.fr>
2020-06-18 11:58:38 +0200

    Ajout lien sur popup confirmation ajout plante dans dashboard

--- app/src/components/plant-card/PlantCard.vue
+++ app/src/components/plant-card/PlantCard.vue
@@ -22,7 +22,7 @@
                         class="alert alert-success alert-dismissible fade show d-none"
                         v-bind:class="{ 'd-block': isAlert }"
                         role="alert"
-                    >La plante a été ajoutée à votre Dashboard !</div>
+                    >La plante a été ajoutée à votre <router-link :to="{ path: `/dashboard/${userLogged.dashboard.id}` }" class="font-weight-bold"> Dashboard </router-link> !</div>
 
                     <router-link :to="{ path: '../plante/detail/' + plant.id }" class="card-title" tag="h5">{{ plant.name }}</router-link>
                     <p class="card-text">{{ plant.description.substring(0, 85) }}...</p>
