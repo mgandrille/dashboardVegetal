18539d7ac9535faf3a5b20fc6070862ebf6b25cd
marie gandrille <mgandrille@yahoo.fr>
2020-06-18 11:48:04 +0200

    Correction Footer

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -2,8 +2,8 @@
 
     <footer class="footer bg-light p-3 row justify-content-between align-items-center mt-5">
         <div class="col-md-10 row align-items-center h-100 mb-3 mb-md-0 text-center text-md-left">
-            <p class="col-md-auto m-md-0 mr-2 p-0"><span class="text-primary font-weight-bold"><i class="fa fa-leaf"></i> &nbsp; Dashboard Végétal</span></p>
-            <p class="font-italic ml-2 col-md-auto m-md-0 p-0">&copy; WF3 équipe 2020 - Carlyne, Marie et Théau</p>
+            <p class="col-md-auto m-md-2 mr-2 p-0"><span class="text-primary font-weight-bold"><i class="fa fa-leaf"></i>&nbsp; GardenBoard  <i class="fa fa-leaf"></i></span> </p>
+            <p class="font-italic ml-2 col-md-auto m-md-2 p-0">   &copy; WF3 équipe 2020 - Carlyne, Marie et Théau</p>
         </div>
         
         <a class="col-md-2 text-center text-md-right"  href="" v-scroll-to="'#top'">Back to top</a>
